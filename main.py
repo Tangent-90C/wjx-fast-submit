9fa3517bf475d3f78c5849363042f646f1ad0280
1759749908 <jianuochenkclo3@gmail.com>
2020-04-15 10:52:36 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get('https://ks.wjx.top/jq/71431917.aspx')
-questions = driver.find_elements_by_css_selector('.div_question')
-
 
 def translate(choose):
     if choose == 'A' or choose == 'a':
@@ -24,46 +20,46 @@ def translate(choose):
         pass
 
 
-def submit_choice(question_number, choose):
+def submit_choice(question_number, choose):    #选择题
 
     answer = questions[question_number].find_elements_by_css_selector('li')
     answer[translate(choose)].click()
 
 
-def submit_multi_choice(question_number, choose):
+def submit_multi_choice(question_number, choose):    #多选题
     pass
     answer = questions[question_number].find_elements_by_css_selector('li')
     for i in range(len(choose)):
         answer[translate(choose[i])].click()
 
 
-def submit_text(question_number, text):
+def submit_text(question_number, text):    #填空题
     text_area = questions[question_number].find_element_by_css_selector(
         'textarea')
     text_area.send_keys(text)
 
 
-def submit_multi_text(question_number, answers_list):
+def submit_multi_text(question_number, answers_list):    #多空填空题
     text_area = questions[question_number].find_elements_by_class_name(
         'underline')
     for i in range(len(answers_list)):
         text_area[i].send_keys(answers_list[i])
 
 
-def next_page():
+def next_page():    #翻页
     next_button = driver.find_element_by_css_selector('#btnNext')
     next_button.click()
     questions = driver.find_elements_by_css_selector('.div_question')
 
 
 def main():
-    submit_text(0, '陈教授')
+    submit_text(0, '李狗蛋')
     submit_choice(1, 'A')
     submit_multi_choice(2, ['a', 'b', 'c', 'e'])
     submit_choice(3, 'A')
-    submit_text(4, '陈教授')
+    submit_text(4, '司马光')
     submit_multi_text(5, ['床前明月光', '举头望明月'])
-    submit_text(6, 'Unknown')
+    submit_text(6, '打119')
     next_page()
     submit_choice(7, 'A')
 
@@ -71,10 +67,14 @@ def main():
     submit_button = driver.find_element_by_css_selector('#submit_button')
     submit_button.click()
 
-    input()
+    input() #等待用户操作后再关闭浏览器
     driver.close()
+
+
+driver = webdriver.Chrome()
+driver.get('https://ks.wjx.top/jq/71431917.aspx')
+questions = driver.find_elements_by_css_selector('.div_question')
 
 
 if __name__ == "__main__":
     main()
-#test
